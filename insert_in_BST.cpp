cd2bfcc71a2a617f1a65815972639df51248f526
Shafayathub <shafayat.ms@gmail.com>
2025-02-12 12:49:14 +0600

    BST Insert example

---
+++ insert_in_BST.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    // constructor
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    val == -1 ? root = NULL : root = new Node(val);
+
+    queue<Node *> q;
+    if (root)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+        int l, r;
+        cin >> l >> r;
+        Node *newLeft, *newRight;
+        l == -1 ? newLeft = NULL : newLeft = new Node(l);
+        r == -1 ? newRight = NULL : newRight = new Node(r);
+
+        p->left = newLeft;
+        p->right = newRight;
+
+        if (p->left)
+        {
+            q.push(p->left);
+        }
+        if (p->right)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+void print_level_order(Node *root){
+    queue<Node *> q;
+    q.push(root);
+    while(!q.empty()){
+        Node *f =q.front();
+        q.pop();
+
+        cout << f->val << " ";
+
+        if(f->left){
+            q.push(f->left);
+        }
+        if(f->right){
+            q.push(f->right);
+        }
+    }
+   
+}
+
+void insert(Node *&root, int val){
+    if(root == NULL){
+        root = new Node(val);
+    }
+    if(root->val > val){
+        if(root->left == NULL){
+            root->left = new Node(val);
+        }
+        else{
+            insert(root->left, val);
+        }
+    }else{
+        if(root->right==NULL){
+            root->right = new Node(val);
+        }else{
+            insert(root->right, val);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = input_tree();
+    int val;
+    cin >> val;
+    insert(root, val);
+    print_level_order(root);
+
+    return 0;
+}
